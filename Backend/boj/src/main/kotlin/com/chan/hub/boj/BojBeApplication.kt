37836c7112577baa204c89efee897483f7316d5e
이찬형 <cksgud410@gmail.com>
2024-05-06 22:33:44 +0900

    [Init] Init Spring Server

---
+++ Backend/boj/src/main/kotlin/com/chan/hub/boj/BojBeApplication.kt
@@ -0,0 +1,11 @@
+package com.chan.hub.boj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BojBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BojBeApplication>(*args)
+}
---
+++ Backend/boj/src/test/kotlin/com/chan/hub/boj/BojBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chan.hub.boj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BojBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
