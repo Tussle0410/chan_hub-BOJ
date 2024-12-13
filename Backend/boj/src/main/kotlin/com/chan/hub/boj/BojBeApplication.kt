6e517b4eaf6dda700435a5966f4ec808efe20d6e
이찬형 <cksgud410@gmail.com>
2024-12-14 01:52:38 +0900

    [Feature] 현재 Application 정보 24시간마다 최신화 진행

--- Backend/boj/src/main/kotlin/com/chan/hub/boj/BojBeApplication.kt
+++ Backend/boj/src/main/kotlin/com/chan/hub/boj/BojBeApplication.kt
@@ -3,8 +3,10 @@ package com.chan.hub.boj
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@EnableScheduling
 class BojBeApplication
 
 fun main(args: Array<String>) {
--- Backend/boj/src/main/kotlin/com/chan/hub/boj/info/adapter/out/ScrapingInfoAdapter.kt
+++ Backend/boj/src/main/kotlin/com/chan/hub/boj/info/adapter/out/ScrapingInfoAdapter.kt
@@ -1,15 +1,33 @@
 package com.chan.hub.boj.info.adapter.out
 
+import com.chan.hub.boj.config.logger
 import com.chan.hub.boj.info.domain.Info
 import com.chan.hub.boj.info.dto.response.ScrapingApplicationInfoResponse
 import com.chan.hub.boj.info.port.out.ScrapingInfoPort
 import com.chan.hub.boj.util.ScrapingUtil
+import jakarta.annotation.PostConstruct
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class ScrapingInfoAdapter(val scrapingUtil: ScrapingUtil) : ScrapingInfoPort {
+    private val log = logger()
+    private var problemCount : Int = 0;
+
+    @PostConstruct
+    fun init() {
+        scrapApplicationInfo()
+        log.info("ScrapingInfoAdapter initialized - problemCount: $problemCount")
+    }
+
     override fun findApplicationInfo(): Info {
+        return Info(problemCount)
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60 * 60 * 24)
+    fun scrapApplicationInfo() {
         val scrapInfo : ScrapingApplicationInfoResponse = ScrapingApplicationInfoResponse.ofDocument(scrapingUtil.scrapApplicationInfo())
-        return scrapInfo.toInfo()
+        problemCount = scrapInfo.solvedProblemCount
+        log.info("ScrapingInfoAdapter Update Patch - problemCount: $problemCount")
     }
 }
