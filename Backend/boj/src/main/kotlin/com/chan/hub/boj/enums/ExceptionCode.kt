3f12b602abaf415cb5774157ab0e81caf18866d1
이찬형 <cksgud410@gmail.com>
2024-12-16 22:42:23 +0900

    [Feature] Exception Code 서버 오류 Status 변경

--- Backend/boj/src/main/kotlin/com/chan/hub/boj/enums/ExceptionCode.kt
+++ Backend/boj/src/main/kotlin/com/chan/hub/boj/enums/ExceptionCode.kt
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus
 
 enum class ExceptionCode (val status: HttpStatus, val code : String, val message: String){
     PROBLEM_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON-001", "문제 풀이를 찾을 수 없습니다."),
-    SCRAPING_ERROR(HttpStatus.NOT_FOUND, "COMMON-002", "데이터를 가져오는 것을 실패하였습니다."),
+    SCRAPING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-002", "데이터를 가져오는 것을 실패하였습니다."),
 }
